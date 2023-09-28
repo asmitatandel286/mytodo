bd09de515c311ea194c2c0d041a68c7381df435b
asmitatandel286 <142808312+asmitatandel286@users.noreply.github.com>
2023-09-28 12:59:21 +0530

    Add files via upload

---
+++ src/components/Task.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Task = ({ task, onDelete, onEdit, onComplete }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTask, setEditedTask] = useState(task.title);
+
+  const handleEdit = () => {
+    onEdit(task.id, editedTask);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="task">
+      {isEditing ? (
+        <div className='edit'>
+          <input
+            type="text"
+            value={editedTask}
+            onChange={(e) => setEditedTask(e.target.value)}
+          />
+          <button onClick={handleEdit}>Save</button>
+        </div>
+      ) : (
+        <div className='box'>
+          <li
+            className={task.completed ? 'completed' : ''}
+            onClick={() => onComplete(task.id)}
+          >
+            {task.title}
+          </li>
+          <div className='btn1'>
+          <button onClick={() => setIsEditing(true)}>Edit</button>
+          <button onClick={() => onDelete(task.id)}>Delete</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Task;
